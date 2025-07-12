3897c79b44d34942586ea3210e8014d3be48d6cb
maziazi <mazis9651@gmail.com>
2025-07-12 23:34:53 +0700

    Initial Commit

---
+++ SoftAnchorCameraToCharacterTests/SoftAnchorCameraToCharacterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoftAnchorCameraToCharacterTests.swift
+//  SoftAnchorCameraToCharacterTests
+//
+//  Created by Muhamad Azis on 12/07/25.
+//
+
+import Testing
+@testable import SoftAnchorCameraToCharacter
+
+struct SoftAnchorCameraToCharacterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
